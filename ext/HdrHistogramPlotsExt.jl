1936b331b2cda848889fc25b08aed5e8fee21c79
Darryl Gamroth <dgamroth@rubus.ca>
2026-01-05 16:57:47 -0800

    Use extensions/weakdeps for Plots integration

---
+++ ext/HdrHistogramPlotsExt.jl
@@ -0,0 +1,18 @@
+module HdrHistogramPlotsExt
+
+using HdrHistogram
+using Plots
+
+function HdrHistogram.percentile_plot(h::HdrHistogram.AbstractHistogram; ticks_per_half_distance::Int64=5, value_scale::Float64=1.0,
+    xlabel::AbstractString="Percentile", ylabel::AbstractString="Value", kwargs...)
+    percentiles, values = HdrHistogram.percentile_plot_data(h; ticks_per_half_distance=ticks_per_half_distance, value_scale=value_scale)
+    return plot(percentiles, values; xlabel=xlabel, ylabel=ylabel, kwargs...)
+end
+
+function HdrHistogram.percentile_plot!(plot_obj, h::HdrHistogram.AbstractHistogram; ticks_per_half_distance::Int64=5, value_scale::Float64=1.0,
+    kwargs...)
+    percentiles, values = HdrHistogram.percentile_plot_data(h; ticks_per_half_distance=ticks_per_half_distance, value_scale=value_scale)
+    return plot!(plot_obj, percentiles, values; kwargs...)
+end
+
+end
--- src/HdrHistogram.jl
+++ src/HdrHistogram.jl
@@ -1,7 +1,6 @@
 module HdrHistogram
 
 using Printf
-using Requires
 
 include("abstracthistogram.jl")
 include("histogram.jl")
@@ -17,9 +16,5 @@ include("logprocessor.jl")
 include("writerreaderphaser.jl")
 include("intervalrecorder.jl")
 include("recorder.jl")
-
-function __init__()
-    @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plotting_plots.jl")
-end
 
 end # module HdrHistogram
